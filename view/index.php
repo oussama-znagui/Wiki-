e821977a39b7231769c82395a665edb15b0abc0f
oussama <znagui.info@gmail.com>
2024-01-08 11:31:53 +0100

    page des article

--- view/index.php
+++ view/index.php
@@ -12,7 +12,10 @@
     <header class=" bg-gradient-to-tr from-rose-100 to-teal-100 h-screen flex flex-col justify-around items-center">
         <!-- <h1 class="mb-4 text-3xl font-black text-gray-900  md:text-5xl lg:text-4xl">Wikis</h1> -->
         <nav class="flex justify-between items-center w-3/4">
-            <span class="text-1xl font-extrabold text-gray-900  md:text-2xl lg:text-3xl">Wikis</span>
+            <span class="flex text-1xl font-extrabold text-gray-900  md:text-2xl lg:text-3xl">Wikis
+                <div class="w-2 h-2 rounded-full bg-green-700">
+                </div>
+            </span>
             <div class="flex items-center gap-6">
                 <a class="text-gray-700 hover:text-orange-600" aria-label="Visit TrendyMinds LinkedIn" href="" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5">
                         <path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">
@@ -97,6 +100,7 @@
 
 
         </div>
+
     </section>
 </body>
 
