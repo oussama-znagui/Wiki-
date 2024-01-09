97c921806028257b700fa4d2c44709e0f41d28fd
oussama <znagui.info@gmail.com>
2024-01-09 09:10:22 +0100

    creation de la base de données

---
+++ file.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE wikis CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+CREATE TABLE users
+(
+   id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fullName VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(255),
+    role int
+)
+CREATE TABLE categories
+(
+   id_cat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    categorie VARCHAR(100),
+    description TEXT
+)
+CREATE TABLE wikis
+(
+   id_wiki INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    titre VARCHAR(100),
+    content TEXT,
+    image VARCHAR(255),
+    creationDate DATE,
+    id_user int,
+    id_cat int,
+    FOREIGN KEY (id_user) REFERENCES users (id_user)
+    FOREIGN KEY (id_cat) REFERENCES categories (id_cat)
+)
+CREATE TABLE tags
+(
+   id_tag INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    tag VARCHAR(100),
+)
+
+
